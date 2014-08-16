f1ab4bdfb1cb0c170271a5fce9f3289b49fee372
Andreas Rudolph <andy@openindex.de>
2014-08-16 23:38:12 +0200

    license headers updated

--- src/include/class.map_osm.php
+++ src/include/class.map_osm.php
@@ -20,7 +20,7 @@
  * Website-Export, Umkreiskarte, bereitgestellt von OpenStreetMap.org.
  *
  * @author Andreas Rudolph & Walter Wagner
- * @copyright 2009-2013, OpenEstate.org
+ * @copyright 2009-2014, OpenEstate.org
  * @license http://www.gnu.org/licenses/gpl-3.0.txt
  * @link http://www.openstreetmap.org/
  */
