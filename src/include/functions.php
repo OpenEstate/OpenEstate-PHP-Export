96e118a63b51fd92f1d36857b4fbd427fb3ff313
Andreas Rudolph <andy@openindex.de>
2017-05-14 06:13:34 +0200

    set version 1.6.36

--- src/include/functions.php
+++ src/include/functions.php
@@ -28,7 +28,7 @@ if (!defined('IN_WEBSITE')) {
   exit;
 }
 
-define('IMMOTOOL_SCRIPT_VERSION', '1.6.35');
+define('IMMOTOOL_SCRIPT_VERSION', '1.6.36');
 //error_reporting( E_ALL );
 //ini_set('display_errors','1');
 
